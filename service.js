0e0b8bdf5fed54c8a73de27fc6de052fc3a88fcf
whitneypage <wpagegarner@gmail.com>
2015-04-16 11:48:33 -0600

    Done

--- service.js
+++ service.js
@@ -2,11 +2,19 @@ var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
 
-
+  var deferred = $q.defer();
   this.getUsers = function() {
-    return $http({
+     $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response) {
+    	console.log(response);
+    	response = response.data.data
+    	for (var i = 0; i < response.length; i++) {
+    		response[i].first_name = 'Ralf'
+    	}
+    	deferred.resolve(response)
     })
+    return deferred.promise;
   }
 });
